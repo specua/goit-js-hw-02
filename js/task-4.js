964dc1ee56589bd2d9b918f2caed6f52ac56f7e0
specua <kalashnikov.kiril8@gmail.com>
2026-01-14 11:06:13 +0200

    global fix task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,18 +1,27 @@
 function getShippingCost(country) {
-    switch (country) {
-        case "China":
-            return `Shipping to ${country} will cost 100 credits`;
-        case "Chile":
-            return `Shipping to ${country} will cost 250 credits`;
-        case "Australia":
-            return `Shipping to ${country} will cost 170 credits`;
-        case "Jamaica":
-            return `Shipping to ${country} will cost 120 credits`;
-        default:
-            return "Sorry, there is no delivery to your country";
-    }
+  let price;
+
+  switch (country) {
+    case "China":
+      price = 100;
+      break;
+    case "Chile":
+      price = 250;
+      break;
+    case "Australia":
+      price = 170;
+      break;
+    case "Jamaica":
+      price = 120;
+      break;
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
+
+  return `Shipping to ${country} will cost ${price} credits`;
 }
 
+// Код для перевірки (залишити для ментора)
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
 console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
